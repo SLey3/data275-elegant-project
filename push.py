f1c1ebde258583e993523fe268d597b7baea9ed0
SLey3 <ghub4127@gmail.com>
2025-05-02 02:32:08 -0700

    "problem 1 completed | added data.py: parsing and processing of data files"

--- push.py
+++ push.py
@@ -3,21 +3,32 @@ USAGE: in the terminal, make sure your in the root directory of the project and 
 python3 push.py "<insert commit message>"
 """
 
-from subprocess import run, PIPE 
+from subprocess import run, PIPE
+from time import sleep
 import sys
 
 
 def main():
-    commit_msg: str = sys.argv[1]
+    try:
+        commit_msg: str = sys.argv[1]
+    except IndexError:
+        print('Commit message is required! \n\nUSAGE: python3 push.py "<insert commit message>"')
+        sys.exit(1)
     
-    print("adding project to git stage")
+    print("adding project to git stage...")
+    sleep(1)
     run(["git", "add", "."], stdin=sys.stdin, stderr=sys.stderr, stdout=PIPE)
     
     print("committing changes...")
+    sleep(1)
     run(["git", "commit", "-m", f'"{commit_msg}"'], stdin=sys.stdin, stderr=sys.stderr, stdout=PIPE)
     
-    print("pushing changes. If any error pops up run 'git pull' else if you cant figure it out check out your error online or with sergio")
+    print("pushing changes. If any error pops up run 'git pull' else if you cant figure it out reach out for support!")
+    sleep(1)
     run(["git", "push"], stdin=sys.stdin, stderr=sys.stderr, stdout=PIPE)
+    
+    print("Changes were pushed successfully! Exiting program...")
+    sleep(1)
     
 if __name__ == '__main__':
     main()
